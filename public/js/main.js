2d3f82562375ef88af9acbf2156aceb438a30441
Alex Pogrebnjak <cah4o.pan4o@gmail.com>
2018-11-08 13:39:55 +0200

    760px add

--- public/js/main.js
+++ public/js/main.js
@@ -22,22 +22,27 @@ function initPage() {
         fixedElements: '#header,#footer-socials,#footer-callback, #fixed-bg',
         anchors: ['main', 'price', 'documents', 'about','contacts'],
         menu: '#myMenu',
+        offsetSections: true,
+        scrollOverflow: true,
         afterLoad: function() {
             $('nav').tab("show");
         }
     });
     $('.menu-mobile').click(function () {
-    	$('.fader').show();
-    	$('#myMenu').addClass('show-menu');
+        $(this).toggleClass('back');
+    	$('.fader').toggleClass('show');
+    	$('#myMenu').toggleClass('show-menu');
     });
     $('.fader').click(function () {
-    	$('.fader').hide();
+    	$('.fader').removeClass('show');
     	$('#myMenu').removeClass('show-menu');
+        $('.menu-mobile').removeClass('back');
     });
     $('#myMenu a').click(function() {
     	$('#myMenu').removeClass('show-menu');
-    	$('.fader').hide();
-    })
+    	$('.fader').removeClass('show');
+        $('.menu-mobile').removeClass('back');
+    });
 
     $('.tabbable .main-description .link').click(function() {
         $(this).parents().find('.main-description').hide();
@@ -71,32 +76,32 @@ function initPage() {
         $(this).parents().find('.detail-block').hide();
         $('.detail-block .page-title').removeClass('hide');
     });
-    $('body').on('shown.bs.tab', function(event) {
-        if ($(window).width() < 992) {
-            $('.telephone, .servers, .team-holder').addClass('swiper-container');
-            var swiper = new Swiper('.telephone.swiper-container', {
-                slidesPerView: 'auto',
-                centeredSlides: true,
-                loop: true,
-                slidesPerGroup: 1,
-                freeMode: true,
-                spaceBetween: 10
-            });
-            var swiper = new Swiper('.servers.swiper-container', {
-                slidesPerView: 'auto',
-                centeredSlides: true,
-                loop: true,
-                freeMode: true,
-                slidesPerGroup: 1,
-                spaceBetween: 10
-            });
-            var swiper = new Swiper('.team-holder.swiper-container', {
-                slidesPerView: 4,
-                loop: false,
-                spaceBetween: 20
-            });
-        }
-    });
+    // $('body').on('shown.bs.tab', function(event) {
+    //     if ($(window).width() < 992) {
+    //         $('.telephone, .servers, .team-holder').addClass('swiper-container');
+    //         var swiper = new Swiper('.telephone.swiper-container', {
+    //             slidesPerView: 'auto',
+    //             centeredSlides: true,
+    //             loop: true,
+    //             slidesPerGroup: 1,
+    //             freeMode: true,
+    //             spaceBetween: 10
+    //         });
+    //         var swiper = new Swiper('.servers.swiper-container', {
+    //             slidesPerView: 'auto',
+    //             centeredSlides: true,
+    //             loop: true,
+    //             freeMode: true,
+    //             slidesPerGroup: 1,
+    //             spaceBetween: 10
+    //         });
+    //         var swiper = new Swiper('.team-holder.swiper-container', {
+    //             slidesPerView: 4,
+    //             loop: false,
+    //             spaceBetween: 20
+    //         });
+    //     }
+    // });
     if ($(window).width() < 992) {
         $('.internet').addClass('swiper-container');
         var swiper = new Swiper('.internet.swiper-container', {
