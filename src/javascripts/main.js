138e6b47f422bbb65725bc8316bb80f8243e46f5
Alex Pogrebnjak <cah4o.pan4o@gmail.com>
2018-11-15 23:30:27 +0200

    add preload

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,23 @@
 $(document).ready(initPage);
 
 function initPage() {
+    var counter = 0;
+    function updateCounter(){
+        if(counter == 101){
+            clearInterval(foo);
+            $('.loadingpage').addClass("pageisloaded");
+        }
+        else{
+            $('.counter span').html(counter);
+            counter++;
+        }
+    }
+    var foo = setInterval(updateCounter , 50);
     $('#fullpage').fullpage({
         responsiveHeight: '570',
         responsiveWidth: '769',
         licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-        fixedElements: '#header,#footer-socials,#footer-callback, #fixed-bg',
+        fixedElements: '#header,.footer, #fixed-bg',
         anchors: ['main', 'price', 'documents', 'about','contacts'],
         menu: '#myMenu',
         offsetSections: true,
@@ -63,32 +75,6 @@ function initPage() {
         $(this).parents().find('.detail-block').hide();
         $('.detail-block .page-title').removeClass('hide');
     });
-    // $('body').on('shown.bs.tab', function(event) {
-    //     if ($(window).width() < 992) {
-    //         $('.telephone, .servers, .team-holder').addClass('swiper-container');
-    //         var swiper = new Swiper('.telephone.swiper-container', {
-    //             slidesPerView: 'auto',
-    //             centeredSlides: true,
-    //             loop: true,
-    //             slidesPerGroup: 1,
-    //             freeMode: true,
-    //             spaceBetween: 10
-    //         });
-    //         var swiper = new Swiper('.servers.swiper-container', {
-    //             slidesPerView: 'auto',
-    //             centeredSlides: true,
-    //             loop: true,
-    //             freeMode: true,
-    //             slidesPerGroup: 1,
-    //             spaceBetween: 10
-    //         });
-    //         var swiper = new Swiper('.team-holder.swiper-container', {
-    //             slidesPerView: 4,
-    //             loop: false,
-    //             spaceBetween: 20
-    //         });
-    //     }
-    // });
     if ($(window).width() < 992) {
         $('.internet').addClass('swiper-container');
         var swiper = new Swiper('.internet.swiper-container', {
@@ -100,5 +86,4 @@ function initPage() {
             spaceBetween: 10
         });
     }
-
 }
